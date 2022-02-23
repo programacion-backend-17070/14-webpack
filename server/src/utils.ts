d9aa16096a457624d5fe626580ce8ec7fecec150
lalomx <lalo0892@gmail.com>
2022-02-23 07:13:00 -0600

    clase en vivo

--- server/src/utils.ts
+++
@@ -1,3 +0,0 @@
-import moment from "moment"
-
-export const getTime = () => moment().format("LLLL")
--- ui/src/app.js
+++
@@ -1,9 +0,0 @@
-import Clock from "./clock"
-
-const span = document.getElementById("clock")
-
-Clock.init((time) => {
-  span.innerText = time
-})
-
-
